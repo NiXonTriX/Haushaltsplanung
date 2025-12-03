a91301e40870a5b7e32aaf5c0f868e2bd7b46e8a
Manuel Hartmann <manuel.hartmann@hilti.com>
2025-12-03 19:21:38 +0100

    refactoring

--- startbootstrap-sb-admin-2-gh-pages/js/household.js
+++ startbootstrap-sb-admin-2-gh-pages/js/household.js
@@ -13,4 +13,65 @@
       item.classList.add('active');
     }
   });
+ if (activePage === 'dashboard') {
+    initCategoryGroupsChart();
+  }
+
+  function initCategoryGroupsChart() {
+    const chartContainer = document.getElementById('category-groups-chart');
+
+    if (!chartContainer || typeof echarts === 'undefined') {
+      return;
+    }
+
+    const categoryGroupChart = echarts.init(chartContainer);
+
+    const option = {
+      angleAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      },
+      radiusAxis: {},
+      polar: {},
+      series: [
+        {
+          type: 'bar',
+          data: [1, 2, 3, 4, 3, 5, 1],
+          coordinateSystem: 'polar',
+          name: 'Manuel',
+          stack: 'a',
+          emphasis: {
+            focus: 'series'
+          }
+        },
+        {
+          type: 'bar',
+          data: [2, 4, 6, 1, 3, 2, 1],
+          coordinateSystem: 'polar',
+          name: 'Stefan',
+          stack: 'a',
+          emphasis: {
+            focus: 'series'
+          }
+        },
+        {
+          type: 'bar',
+          data: [1, 2, 3, 4, 1, 2, 5],
+          coordinateSystem: 'polar',
+          name: 'Berkant',
+          stack: 'a',
+          emphasis: {
+            focus: 'series'
+          }
+        }
+      ],
+      legend: {
+        show: true,
+        data: ['Manuel', 'Stefan', 'Berkant']
+      }
+    };
+
+    categoryGroupChart.setOption(option);
+    window.addEventListener('resize', () => categoryGroupChart.resize());
+  }
 })();
