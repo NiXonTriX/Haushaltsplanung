0cc4b8b87ff333b98e2e523105b3c204b457caf3
Manuel Hartmann <manuel.hartmann@hilti.com>
2025-12-04 09:31:53 +0100

    Implement SCSS

--- startbootstrap-sb-admin-2-gh-pages/gulpfile.js
+++ startbootstrap-sb-admin-2-gh-pages/gulpfile.js
@@ -1,40 +1,32 @@
 "use strict";
 
-/* --------------------------------------------------
- * Plugins laden
- * -------------------------------------------------- */
-const gulp         = require("gulp");
-const plumber      = require("gulp-plumber");
-const browsersync  = require("browser-sync").create();
-const del          = require("del");
-const rename       = require("gulp-rename");
-const terser       = require("gulp-terser");
-const header       = require("gulp-header");
-const sassCompiler = require("gulp-sass")(require("sass"));
-const cleanCSS     = require("gulp-clean-css");
+// Load plugins
 const autoprefixer = require("gulp-autoprefixer");
-const merge        = require("merge-stream");
+const browsersync = require("browser-sync").create();
+const cleanCSS = require("gulp-clean-css");
+const del = require("del");
+const gulp = require("gulp");
+const header = require("gulp-header");
+const merge = require("merge-stream");
+const plumber = require("gulp-plumber");
+const rename = require("gulp-rename");
+const sass = require("gulp-sass")(require("sass"));
+const terser = require("gulp-terser");
 
-// eigener HTML-Build (optional, wie gehabt)
-const { buildAll } = require("./scripts/build-html");
 
-// package.json laden für Banner
-const pkg = require("./package.json");
+// Load package.json for banner
+const pkg = require('./package.json');
 
-/* --------------------------------------------------
- * Banner
- * -------------------------------------------------- */
-const banner = [
-  "/*!\n",
-  ` * Start Bootstrap - ${pkg.title} v${pkg.version} (${pkg.homepage})\n`,
-  ` * Copyright 2013-${new Date().getFullYear()} ${pkg.author}\n`,
-  ` * Licensed under ${pkg.license} (https://github.com/StartBootstrap/${pkg.name}/blob/master/LICENSE)\n`,
-  " */\n\n",
-].join("");
+// Set the banner content
+const banner = ['/*!\n',
+  ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
+  ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
+  ' * Licensed under <%= pkg.license %> (https://github.com/StartBootstrap/<%= pkg.name %>/blob/master/LICENSE)\n',
+  ' */\n',
+  '\n'
+].join('');
 
-/* --------------------------------------------------
- * BrowserSync starten
- * -------------------------------------------------- */
+// BrowserSync
 function browserSync(done) {
   browsersync.init({
     server: {
@@ -45,129 +37,111 @@ function browserSync(done) {
   done();
 }
 
-/* --------------------------------------------------
- * HTML Task
- * -------------------------------------------------- */
-async function html() {
-  await buildAll();
-  browsersync.reload();
-}
-
-/* --------------------------------------------------
- * BrowserSync Reload
- * -------------------------------------------------- */
+// BrowserSync reload
 function browserSyncReload(done) {
   browsersync.reload();
   done();
 }
 
-/* --------------------------------------------------
- * Vendor Ordner löschen
- * -------------------------------------------------- */
+// Clean vendor
 function clean() {
   return del(["./vendor/"]);
 }
 
-/* --------------------------------------------------
- * Third-party Dependencies kopieren
- * -------------------------------------------------- */
+// Bring third party dependencies from node_modules into vendor directory
 function modules() {
   // Bootstrap JS
-  const bootstrapJS = gulp
-    .src("./node_modules/bootstrap/dist/js/*")
-    .pipe(gulp.dest("./vendor/bootstrap/js"));
-
+  var bootstrapJS = gulp.src('./node_modules/bootstrap/dist/js/*')
+    .pipe(gulp.dest('./vendor/bootstrap/js'));
   // Bootstrap SCSS
-  const bootstrapSCSS = gulp
-    .src("./node_modules/bootstrap/scss/**/*")
-    .pipe(gulp.dest("./vendor/bootstrap/scss"));
-
+  var bootstrapSCSS = gulp.src('./node_modules/bootstrap/scss/**/*')
+    .pipe(gulp.dest('./vendor/bootstrap/scss'));
   // ChartJS
-  const chartJS = gulp
-    .src("./node_modules/chart.js/dist/*.js")
-    .pipe(gulp.dest("./vendor/chart.js"));
-
-  // Datatables
-  const dataTables = gulp
-    .src([
-      "./node_modules/datatables.net/js/*.js",
-      "./node_modules/datatables.net-bs4/js/*.js"
+  var chartJS = gulp.src('./node_modules/chart.js/dist/*.js')
+    .pipe(gulp.dest('./vendor/chart.js'));
+  // dataTables
+  var dataTables = gulp.src([
+      './node_modules/datatables.net/js/*.js',
+      './node_modules/datatables.net-bs4/js/*.js',
+      './node_modules/datatables.net-bs4/css/*.css'
     ])
-    .pipe(gulp.dest("./vendor/datatables"));
-
-  return merge(bootstrapJS, bootstrapSCSS, chartJS, dataTables);
+    .pipe(gulp.dest('./vendor/datatables'));
+  // Font Awesome
+  var fontAwesome = gulp.src('./node_modules/@fortawesome/**/*')
+    .pipe(gulp.dest('./vendor'));
+  // jQuery Easing
+  var jqueryEasing = gulp.src('./node_modules/jquery.easing/*.js')
+    .pipe(gulp.dest('./vendor/jquery-easing'));
+  // jQuery
+  var jquery = gulp.src([
+      './node_modules/jquery/dist/*',
+      '!./node_modules/jquery/dist/core.js'
+    ])
+    .pipe(gulp.dest('./vendor/jquery'));
+  return merge(bootstrapJS, bootstrapSCSS, chartJS, dataTables, fontAwesome, jquery, jqueryEasing);
 }
 
-/* --------------------------------------------------
- * CSS Task
- * -------------------------------------------------- */
+// CSS task
 function css() {
   return gulp
     .src("./scss/**/*.scss")
     .pipe(plumber())
-    .pipe(
-      sassCompiler({
-        outputStyle: "expanded"
-      }).on("error", sassCompiler.logError)
-    )
-    .pipe(autoprefixer())
+    .pipe(sass({
+      outputStyle: "expanded",
+      includePaths: "./node_modules",
+    }))
+    .on("error", sass.logError)
+    .pipe(autoprefixer({
+      cascade: false
+    }))
+    .pipe(header(banner, {
+      pkg: pkg
+    }))
     .pipe(gulp.dest("./css"))
+    .pipe(rename({
+      suffix: ".min"
+    }))
     .pipe(cleanCSS())
-    .pipe(
-      rename({
-        suffix: ".min"
-      })
-    )
     .pipe(gulp.dest("./css"))
     .pipe(browsersync.stream());
 }
 
-/* --------------------------------------------------
- * JS Task
- * -------------------------------------------------- */
+// JS task
 function js() {
   return gulp
     .src([
-      "./js/**/*.js",
-      "!./js/**/*.min.js"
+      './js/*.js',
+      '!./js/*.min.js',
     ])
-    .pipe(plumber())
-    .pipe(terser()) // <<< moderner Minifier
-    .pipe(header(banner, { pkg }))
-    .pipe(
-      rename({
-        suffix: ".min"
-      })
-    )
-    .pipe(gulp.dest("./js"))
+    .pipe(terser())
+    .pipe(header(banner, {
+      pkg: pkg
+    }))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('./js'))
     .pipe(browsersync.stream());
 }
 
-/* --------------------------------------------------
- * Watch Dateien
- * -------------------------------------------------- */
+// Watch files
 function watchFiles() {
   gulp.watch("./scss/**/*", css);
-  gulp.watch(["./js/**/*.js", "!./js/**/*.min.js"], js);
-  gulp.watch("./src/**/*.html", html);
-  gulp.watch(["./*.html", "./img/**/*"], browserSyncReload);
+  gulp.watch(["./js/**/*", "!./js/**/*.min.js"], js);
+  gulp.watch("./**/*.html", browserSyncReload);
 }
 
-/* --------------------------------------------------
- * Tasks definieren
- * -------------------------------------------------- */
+// Define complex tasks
 const vendor = gulp.series(clean, modules);
-const build  = gulp.series(vendor, gulp.parallel(css, js, html));
-const watch  = gulp.series(build, gulp.parallel(watchFiles, browserSync));
+const build = gulp.series(vendor, gulp.parallel(css, js));
+const watch = gulp.series(build, gulp.parallel(watchFiles, browserSync));
 
-/* --------------------------------------------------
- * Exports
- * -------------------------------------------------- */
-exports.css    = css;
-exports.js     = js;
-exports.clean  = clean;
+// Export tasks
+exports.css = css;
+exports.js = js;
+exports.clean = clean;
 exports.vendor = vendor;
-exports.build  = build;
-exports.html   = html;
-exports.watch  = watch;
+exports.build = build;
+exports.watch = watch;
 exports.default = build;
+ 
